cd815cd42d268ada020679a17c9aeaa8b1787ade
Amal Krishna <amalkrishna329@gmail.com>
2021-09-14 21:11:09 +0530

    Created spring boot app

---
+++ SecurityService/src/main/java/com/ecommerce/security/SecurityService/SecurityServiceApplication.java
@@ -0,0 +1,13 @@
+package com.ecommerce.security.SecurityService;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SecurityServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SecurityServiceApplication.class, args);
+	}
+
+}
---
+++ SecurityService/src/test/java/com/ecommerce/security/SecurityService/SecurityServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ecommerce.security.SecurityService;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SecurityServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
